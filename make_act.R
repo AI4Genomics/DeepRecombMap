7aa3b0b6c03ad8a2a22fd8f749bc8064d015ef9c
Zoe Schmilovich <zoe@beluga1.int.ets1.calculquebec.ca>
2020-04-19 21:51:22 -0400

    add a col for reg

--- make_act.R
+++ make_act.R
@@ -6,11 +6,14 @@ encode <- read.table("encode_roadmap_act_col1.txt")
 newcol <- "DSB"
 DSB[,newcol] <- 1
 
+reg <- "regulatory"
+DSB[,reg] <- 0
+
 encode[,newcol] <- 0
-head(encode)
+encode[,reg] <- 1
 
 df <- rbind(DSB, encode)
 summary(df)
 table(df$DSB)
-
+table(df$reg)
 write.table(df, "activity.txt", sep="\t", quote = FALSE, row.names = FALSE)
