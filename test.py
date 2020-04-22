99808359a3b68cf60bd97f798b6280e5a8925d31
kiristern <kiri.a.stern@gmail.com>
2020-04-21 21:58:29 -0400

    change Basset -> Recomb

--- test.py
+++ test.py
@@ -19,6 +19,6 @@ import logomaker # https://github.com/jbkinney/logomaker/tree/master/logomaker/t
 import argparse
 from pytz import timezone
 
-basset_dataset_test = BassetDataset(path='./data/', f5name='sample_dataset.h5', split='test')
-print("The number of samples in {} split is {}.".format('test', len(basset_dataset_test)))
-print("The first 10 ids of test samples are:\n  {}\n".format("\n  ".join(basset_dataset_test.ids[:10])))
+recomb_dataset_test = RecombDataset(path='./data/', f5name='sample_dataset.h5', split='test')
+print("The number of samples in {} split is {}.".format('test', len(recomb_dataset_test)))
+print("The first 10 ids of test samples are:\n  {}\n".format("\n  ".join(recomb_dataset_test.ids[:10])))
